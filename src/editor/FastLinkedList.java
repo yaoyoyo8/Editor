b7ed4f5e9fbdfcbe2f429ab1ee3ae07d84be4486
Yoly0412 <yoly0412@163.com>
2016-10-03 11:58:54 +0800

    10/2 update editor, keyevent, node, list

--- src/editor/FastLinkedList.java
+++ src/editor/FastLinkedList.java
@@ -1,55 +1,77 @@
 package editor;
 
-import org.w3c.dom.*;
-import javax.xml.soap.SOAPElement;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.Text;
+
+import javafx.scene.text.Text;
 
 /**
  * Created by Yao on 2016/10/1.
  */
 public class FastLinkedList {
-    private class FastLinkedListNode {
-        private Text item;
-        private FastLinkedListNode prev;
-        private FastLinkedListNode next;
-
-        public FastLinkedListNode(Text text, FastLinkedListNode p, FastLinkedListNode n) {
-            item = text;
-            prev = p;
-            next = n;
-        }
-        public Text getItem() {
-            return item;
-        }
-        public FastLinkedListNode getPrev() {
-            return prev;
-        }
-        public FastLinkedListNode getNext() {
-            return next;
-        }
-    }
     private FastLinkedListNode sentinel;
     private int currentIndex;
     private int curX;
     private int curY;
     private FastLinkedListNode currentNode;
+    private int size;
     private int fontSize = 12;
     private String fontName = "Verdana";
 
     public FastLinkedList() {
-
+        sentinel = new FastLinkedListNode(new Text(), null, null, 0);
+        currentIndex = 0;
+        curX = 5;
+        curY = 0;
+        currentNode = sentinel;
+        size = 0;
     }
-    public void add(FastLinkedListNode f) {
-        return;
-    }
-    public void remove() {
-        return;
-    }
+    //get methods
+    public FastLinkedListNode getSentinel(){return sentinel;}
+    public FastLinkedListNode getCurrentNode() {return currentNode;}
     public int getCurX() {
         return curX;
     }
     public int getCurY() {
         return curY;
     }
+    // update current x,y
+    public void updateCurXY(int curX, int curY) {
+        this.curX = curX;
+        this.curY = curY;
+    }
+    //add a node
+    public void add(FastLinkedListNode node) {
+        if (currentIndex == size) {
+            currentNode.setNext(node);
+            node.setPrev(currentNode);
+            node.setNext(null);
+            currentNode = node;
+        } else {
+            node.setNext(currentNode.getNext());
+            currentNode.getNext().setPrev(node);
+            currentNode.setNext(node);
+            node.setPrev(currentNode);
+            currentNode = node;
+        }
+        size++;
+        currentIndex++;
+        int charWidth = (int) Math.round(node.getItem().getLayoutBounds().getWidth());
+        int charHeight = (int) Math.round(node.getItem().getLayoutBounds().getHeight());
+        ///////how to update cur x, y? need to be improved
+        updateCurXY(curX + charWidth, curY);
+    }
+    //remove the current node
+    public FastLinkedListNode remove() {
+        FastLinkedListNode ret = currentNode;
+        int charWidth = (int) Math.round(currentNode.getItem().getLayoutBounds().getWidth());
+        int charHeight = (int) Math.round(currentNode.getItem().getLayoutBounds().getHeight());
+        currentNode.getNext().setPrev(currentNode.getPrev());
+        currentNode.getPrev().setNext(currentNode.getNext());
+        currentNode = currentNode.getPrev();
+        size--;
+        currentIndex--;
+        ///////how to update cur x, y?? need to be improved
+        updateCurXY(curX - charWidth, curY);
+        return ret;
+    }
+
 }
---
+++ src/editor/FastLinkedListNode.java
@@ -0,0 +1,42 @@
+package editor;
+
+import javafx.scene.text.Text;
+
+/**
+ * Created by Yao on 2016/10/3.
+ */
+public class FastLinkedListNode {
+    private Text item;
+    private FastLinkedListNode prev;
+    private FastLinkedListNode next;
+    private int index;
+
+    public FastLinkedListNode() {
+        item = null;
+        prev = null;
+        next = null;
+        index = 0;
+    }
+    public FastLinkedListNode(Text item, FastLinkedListNode prev, FastLinkedListNode next, int index) {
+        this.item = item;
+        this.prev = prev;
+        this.next = next;
+        this.index = index;
+    }
+    public FastLinkedListNode(Text text) {item = text;}
+    public FastLinkedListNode getNext() {return next;}
+    public FastLinkedListNode getPrev() {return prev;}
+    public Text getItem() {return item;}
+    public int getIndex() {return index;}
+    public int getXPos() {
+        return (int) Math.round(item.getX());
+    }
+    public int getYPos() {return (int) Math.round(item.getY());}
+
+    public void setPrev(FastLinkedListNode prev) {this.prev = prev;}
+    public void setNext(FastLinkedListNode next) {this.next = next;}
+    public void setItem(Text item) {this.item = item;}
+    public void setIndex(int index){this.index = index;}
+    public void updateXPos(int newX) {item.setX(newX);}
+    public void updateYPos(int newY) {item.setY(newY);}
+}
